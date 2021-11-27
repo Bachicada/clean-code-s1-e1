f1ce71fe5b02f5c4311458674b291592c43e3ad9
Bachicada <bachicada@gmail.com>
2021-11-27 21:28:21 +0300

    refactor: adjust HTML,js-files  according to rule 1.2 in html-and-css-extended.md

--- app.js
+++ app.js
@@ -51,6 +51,7 @@ var createNewTaskElement=function(taskString){
     deleteButton.className="button delete";
     deleteButtonImg.src="./remove.svg";
     deleteButtonImg.className="delete-bg";
+    deleteButtonImg.alt="Remove button";
     deleteButton.appendChild(deleteButtonImg);
 
 
